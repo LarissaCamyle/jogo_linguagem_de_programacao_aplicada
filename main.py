8ffac4465fb92153847a33649ef4d70ab15a7204
Larissa Camyle da Silva <lsilva@focus.ind.br>
2026-05-25 14:21:57 -0300

    Adicionando arquivos

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
+#FINALIZEI A AULA 1
 import pygame
-
 #inicializa
 pygame.init()
 
-#PAREI NA AULA PRATICA 1 44:44
+
 #criar janela
-window = pygame.display.set_mode(size)
+window = pygame.display.set_mode(size=(800, 500))
+
+while True:
+    #lista com todos os eventos que acontecerem como abrir e fechar janela e interações
+    for event in pygame.event.get():
+        #se o evento de tentar fechar janela acontecer
+        if event.type == pygame.QUIT:
+            #fecha a janela
+            pygame.quit()
+            #encerra o código
+            quit()
+           
 
 
 
